baadbf5da9dc848be3df3534622cd8d431a7449c
陈止风 <yingzinanfei@hotmail.com>
2016-03-27 10:56:54 +0800

    网络预选赛

--- buptoj/热身赛3-A.cpp
+++ buptoj/热身赛3-A.cpp
@@ -1,3 +1,4 @@
+/*
 #include <stdio.h>
 int main() {
 	int n;
@@ -49,4 +50,69 @@ int main() {
 
 		}
 	}
+}
+*/
+
+#include <iostream>
+#include <functional>
+#include <algorithm>
+#include <cstring>
+#include <vector>
+#include <string>
+#include <cstdio>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <set>
+#include <map>
+using namespace std;
+typedef long long ll;
+
+#define INF 0x333f3f3f
+
+const ll mod = 1000000007;
+const int maxn = 1e5 + 5;
+const double PI = acos(-1.0);
+
+int n;
+int val[maxn];
+double dp[2][1025];
+
+void solve()
+{
+	int i, j, k;
+
+	for (i = 1; i <= n; i++)
+	{
+		scanf("%d", &val[i]);
+	}
+	memset(dp, 0, sizeof(dp));
+	int now = 1, pre = 0;
+	dp[pre][0] = 1;
+
+	for (i = 1; i <= n; i++)
+	{
+		memset(dp[now], 0, sizeof(dp[now]));
+		for (j = 0; j <= 128; j++)
+		{
+			for (k = 1; k <= val[i]; k++)
+			{
+				dp[now][j^k] += (dp[pre][j] / (double)val[i]);
+			}
+		}
+		swap(now, pre);
+	}
+
+	printf("%.6lf\n", dp[pre][0]);
+}
+
+int main()
+{
+	while (scanf("%d", &n) != EOF)
+	{
+		solve();
+	}
+	//system("pause");
+	return 0;
 }
